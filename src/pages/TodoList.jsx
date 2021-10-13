754b5ed804b6076ddc6a612d01cbee31f04069bd
Ageng Ayu <70548337+agengayuu@users.noreply.github.com>
2021-10-13 11:31:12 +0700

    Add TodoFrom Components

--- src/pages/TodoList.jsx
+++ src/pages/TodoList.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 
 import Paper from "../components/paper/Paper";
 import Header from "../components/header/Header";
-import TodoForm from "../components/TodoForm";
+import TodoForm from "../components/todo/TodoForm";
 import Todos from "../components/todos/Todos";
 
 const TodoList = () => {
